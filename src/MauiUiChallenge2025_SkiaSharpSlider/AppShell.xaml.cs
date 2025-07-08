fa2b8dde7288b26110a2c6512549f776074b34db
John Bijsterbosch <john.bijsterbosch@live.com>
2025-07-08 06:53:27 +0200

    Added empty .NET MAUI project

---
+++ src/MauiUiChallenge2025_SkiaSharpSlider/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiUiChallenge2025_SkiaSharpSlider;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/MauiUiChallenge2025_SkiaSharpSlider/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiUiChallenge2025_SkiaSharpSlider;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
