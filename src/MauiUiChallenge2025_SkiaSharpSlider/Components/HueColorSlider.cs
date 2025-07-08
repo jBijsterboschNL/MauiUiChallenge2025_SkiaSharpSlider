666a932c570d70582315602e766484ef77544d26
John Bijsterbosch <john.bijsterbosch@live.com>
2025-07-08 09:38:27 +0200

    Step 2.	Creating an empty component and hooking up the OnPaintSurface() method

---
+++ src/MauiUiChallenge2025_SkiaSharpSlider/Components/HueColorSlider.cs
@@ -0,0 +1,12 @@
+using SkiaSharp.Views.Maui;
+using SkiaSharp.Views.Maui.Controls;
+
+namespace MauiUiChallenge2025_SkiaSharpSlider.Components;
+
+public class HueColorSlider : SKCanvasView
+{
+    protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+    {
+        base.OnPaintSurface(e);
+    }
+}
