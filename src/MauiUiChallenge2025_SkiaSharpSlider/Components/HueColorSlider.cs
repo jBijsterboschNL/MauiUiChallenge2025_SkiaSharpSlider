52946ffb0816ed87813f608b56f1b216d46f9684
John Bijsterbosch <john.bijsterbosch@live.com>
2025-07-08 10:18:13 +0200

    Step 3a. Defining const values, storing canvas information and setting up separate draw methods

--- src/MauiUiChallenge2025_SkiaSharpSlider/Components/HueColorSlider.cs
+++ src/MauiUiChallenge2025_SkiaSharpSlider/Components/HueColorSlider.cs
@@ -1,3 +1,4 @@
+using SkiaSharp;
 using SkiaSharp.Views.Maui;
 using SkiaSharp.Views.Maui.Controls;
 
@@ -5,8 +6,43 @@ namespace MauiUiChallenge2025_SkiaSharpSlider.Components;
 
 public class HueColorSlider : SKCanvasView
 {
+    public const double ComponentHeight = 24;
+    private const int SliderBarHeight = 4;
+    private const float ThumbRadius = 12f;
+    private const int ThumbBorderThickness = 4;
+    
+    private SKCanvas? _canvas;
+    private int _actualWidth, _actualHeight;
+
+    public HueColorSlider()
+    {
+        HeightRequest = ComponentHeight;
+    }
+
     protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
     {
         base.OnPaintSurface(e);
+
+        _canvas = e.Surface.Canvas;
+        _actualWidth = e.Info.Width;
+        _actualHeight = e.Info.Height;
+
+        DrawComponent();
+    }
+
+    private void DrawComponent()
+    {
+        _canvas.Clear(SKColors.Transparent);
+
+        DrawSliderBar();
+        DrawThumb();
+    }
+
+    private void DrawSliderBar()
+    {
+    }
+
+    private void DrawThumb()
+    {
     }
 }
