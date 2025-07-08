fa5f1b29f957bf7423d10e8039125b915e870665
John Bijsterbosch <john.bijsterbosch@live.com>
2025-07-08 11:05:54 +0200

    Step 3b. Implement DrawSliderBar()

--- src/MauiUiChallenge2025_SkiaSharpSlider/Components/HueColorSlider.cs
+++ src/MauiUiChallenge2025_SkiaSharpSlider/Components/HueColorSlider.cs
@@ -8,15 +8,24 @@ public class HueColorSlider : SKCanvasView
 {
     public const double ComponentHeight = 24;
     private const int SliderBarHeight = 4;
+    private const int SliderBarRadius = SliderBarHeight / 2;
     private const float ThumbRadius = 12f;
     private const int ThumbBorderThickness = 4;
+    
+    private readonly SKColor[] _colors = new SKColor[8];
     
     private SKCanvas? _canvas;
     private int _actualWidth, _actualHeight;
 
     public HueColorSlider()
     {
+        IgnorePixelScaling = true;
         HeightRequest = ComponentHeight;
+        
+        for (var i = 0; i < _colors.Length; i++)
+        {
+            _colors[i] = SKColor.FromHsl(i * 360f / (_colors.Length - 1), 100, 50);
+        }
     }
 
     protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
@@ -32,6 +41,9 @@ public class HueColorSlider : SKCanvasView
 
     private void DrawComponent()
     {
+        if (_canvas is null)
+            return;
+        
         _canvas.Clear(SKColors.Transparent);
 
         DrawSliderBar();
@@ -40,6 +52,29 @@ public class HueColorSlider : SKCanvasView
 
     private void DrawSliderBar()
     {
+        // calculate the size and position of our slider bar
+        var barMarginY = (_actualHeight - SliderBarHeight) / 2;
+        var barRect = new SKRect(
+            left: ThumbRadius,
+            top: barMarginY,
+            right: _actualWidth - ThumbRadius,
+            bottom: barMarginY + SliderBarHeight);
+        
+        // create painting for our slider bar
+        using SKPaint paint = new()
+        {
+            Style = SKPaintStyle.Fill,
+            IsStroke = false,
+            IsAntialias = true,
+            Shader = SKShader.CreateLinearGradient(
+                new SKPoint(barRect.Left, 0),
+                new SKPoint(barRect.Right, 0),
+                _colors,
+                SKShaderTileMode.Repeat)
+        };
+        
+        // draw the slider bar
+        _canvas.DrawRoundRect(barRect, SliderBarRadius, SliderBarRadius, paint);
     }
 
     private void DrawThumb()
